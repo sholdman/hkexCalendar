7bab87554f694557ebf81306fe727208430ba01e
sholdman <g1153069@study.ouhk.edu.hk>
2020-11-13 11:35:33 +0800

    Update server.js
    adjust for new product in hkex

--- server.js
+++ server.js
@@ -8,11 +8,105 @@ var moment = require('moment');
 var request = require('request');
 var app = express();
 
-var futuresListEng = [["HangSeng Index Futures and Options (Including Flexible Index Options)","Mini-HangSeng Index Futures and Options","HangSeng China Enterprises Index Futures and Options (Including Flexible Index Options)","Mini-HangSeng China Enterprises Index Futures and Options","HangSeng Index Total Return Index Futures ","HangSeng China Enterprises Index Total Return Index Futures","StockFutures and Options","SectorIndex Futures"],["WeeklyHang Seng Index Options","WeeklyHang Seng China Enterprises Index Options"],["HSIDividend Point Index Futures","HSCEIDividend Point Index Futures"],["HSIVolatility Index Futures"],["MSCIAC Asia ex Japan Net Total Return Index Futures"],["CESChina 120 Index Futures"],["-IBOVESPA Futures "],["-MICEX Index Futures"],["-FTSE/JSE Top40 Futures"],["One-MonthHIBOR Futures","Three-MonthHIBOR Futures ","USD/CNHFuturesand Options"],["EUR/CNHFutures","JPY/CNHFutures","AUD/CNHFutures","CNH/USDFutures"],["USD& CNH London Aluminium/Zinc/Copper/Nickel/Tin/LeadMini Futures"],["USD& CNH Gold Futures"],["IronOreFutures"]]
-var futuresListSc = [["恒生指数期货及期权(包括自订条款指数期权)","小型恒生指数期货及期权","恒生中国企业指数期货及期权(包括自订条款指数期权)","小型恒生中国企业指数期货及期权","恒生指数股息累计指数期货","恒生中国企业指数股息累计指数期货","股票期货及期权","行业指数期货"],["每周恒生指数期权","每周恒生中国企业指数期权"],["恒指股息点指数期货","恒生国企股息点指数期货"],["恒指波幅指數期货"],["MSCI亚洲除日本净总回报指数期货"],["中华交易服务中国120 指数期货"],["-IBOVESPA期货"],["-MICEX指数期货"],["-FTSE/JSE Top40期货"],["一个月港元利率期货","三个月港元利率期货","美元兑人民币(香港)期货及期权"],["欧元兑人民币(香港)期货","日圆兑人民币(香港)期货","澳元兑人民币(香港)期货","人民币(香港)兑美元期货"],["美元及人民币(香港)伦敦铝/锌/铜/镍/锡/铅期货小型合约"],["美元及人民币(香港)黄金期货合约"],["铁矿石期货"]]
+var futuresListEng = [["HangSeng Index Futures and Options (Including Flexible Index Options)",
+						"Mini-HangSeng Index Futures and Options",
+						"HangSeng China Enterprises Index Futures and Options (Including Flexible Index Options)",
+						"Mini-HangSeng China Enterprises Index Futures and Options","HangSeng Index Total Return Index Futures ",
+						"HangSeng China Enterprises Index Total Return Index Futures",
+						"StockFutures and Options","SectorIndex Futures"
+						],
+						["WeeklyHang Seng Index Options",
+						"WeeklyHang Seng China Enterprises Index Options"
+						],
+						["HSIDividend Point Index Futures",
+						"HSCEIDividend Point Index Futures"
+						],
+						["HSIVolatility Index Futures"
+						],
+						["MSCIAC Asia ex Japan Net Total Return Index Futures"],
+						["MSCI Australia Net Total Return (USD) Index Futures",
+"MSCI China Free Net Total Return (USD) Index Futures",
+"MSCI EM Asia ex China Net Total Return (USD) Index Futures",
+"MSCI EM Asia ex Korea Net Total Return (USD) Index Futures",
+"MSCI EM Asia Net Total Return (USD) Index Futures",
+"MSCI EM EMEA Net Total Return (USD) Index Futures",
+"MSCI EM ex China Net Total Return (USD) Index Futures",
+"MSCI EM ex Korea Net Total Return (USD) Index Futures",
+"MSCI EM LatAm Net Total Return (USD) Index Futures",
+"MSCI Emerging Markets (USD) Index Futures",
+"MSCI Emerging Markets Net Total Return (USD) Index Futures",
+"MSCI Hong Kong Net Total Return (USD) Index Futures",
+"MSCI India Net Total Return (USD) Index Futures",
+"MSCI Indonesia Net Total Return (USD) Index Futures",
+"MSCI Japan Net Total Return (JPY) Index Futures",
+"MSCI Japan Net Total Return (USD) Index Futures",
+"MSCI Malaysia Net Total Return (USD) Index Futures",
+"MSCI New Zealand Net Total Return (USD) Index Futures",
+"MSCI Pacific ex Japan Net Total Return (USD) Index Futures",
+"MSCI Pacific Net Total Return (USD) Index Futures",
+"MSCI Philippines Net Total Return (USD) Index Futures",
+"MSCI Singapore Free Net Total Return (USD) Index Futures",
+"MSCI Singapore Net Total Return (USD) Index Futures",
+"MSCI Taiwan Net Total Return (USD) Index Futures",
+"MSCI Taiwan 25/50 Net Total Return (USD) Index Futures",
+"MSCI Thailand Net Total Return (USD) Index Futures",
+"MSCI Vietnam Net Total Return (USD) Index Futures"],
+						["MSCI China Free (USD) Index Futures"],
+						["MSCI India (USD) Index Futures"],
+						["MSCI Indonesia (USD) Index Futures"],
+						["MSCI Japan (JPY) Index Futures"],
+						["MSCI Malaysia (USD) Index Futures"],
+						["MSCI Philippines (USD) Index Futures"],
+						["MSCI Singapore Free (SGD) Index Futures"],
+						["MSCI Taiwan (USD) Index Futures",
+							"MSCI Taiwan 25/50 (USD) Index Futures"],
+							["MSCI Thailand (USD) Index Futures"],
+							["MSCI Vietnam (USD) Index Futures"],
+						["CESChina 120 Index Futures"],
+						["-IBOVESPA Futures "],["-MICEX Index Futures"],["-FTSE/JSE Top40 Futures"],["One-MonthHIBOR Futures","Three-MonthHIBOR Futures ","USD/CNHFuturesand Options"],["EUR/CNHFutures","JPY/CNHFutures","AUD/CNHFutures","CNH/USDFutures"],["USD& CNH London Aluminium/Zinc/Copper/Nickel/Tin/LeadMini Futures"],["USD& CNH Gold Futures"],["IronOreFutures"]]
+var futuresListSc = [["恒生指数期货及期权(包括自订条款指数期权)","小型恒生指数期货及期权","恒生中国企业指数期货及期权(包括自订条款指数期权)","小型恒生中国企业指数期货及期权","恒生指数股息累计指数期货","恒生中国企业指数股息累计指数期货","股票期货及期权","行业指数期货"],["每周恒生指数期权","每周恒生中国企业指数期权"],["恒指股息点指数期货","恒生国企股息点指数期货"],["恒指波幅指數期货"],["MSCI亚洲除日本净总回报指数期货"],
+						["MSCI 澳洲净总回报（美元）指数期货",
+						"MSCI 中国自由净总回报（美元）指数期货",
+						"MSCI 新兴市场亚洲除中国净总回报（美元） 指数期货",
+						"MSCI 新兴市场亚洲除韩国净总回报（美元）指数期货",
+						"MSCI 新兴市场亚洲净总回报（美元）指数期货",
+						"MSCI 新兴市场欧非中东净总回报（美元）指数期货",
+						"MSCI 新兴市场除中国净总回报（美元）指数期货",
+						"MSCI 新兴市场除韩国净总回报（美元）指数期货",
+						"MSCI 新兴市场拉丁美洲净总回报（美元）指数期货",
+						"MSCI 新兴市场（美元）指数期货",
+						"MSCI 新兴市场净总回报（美元）指数期货",
+						"MSCI 香港净总回报（美元）指数期货",
+						"MSCI 印度净总回报（美元）指数期货",
+						"MSCI 印尼净总回报（美元）指数期货",
+						"MSCI 日本净总回报（日元）指数期货",
+						"MSCI 日本净总回报（美元）指数期货",
+						"MSCI 马来西亚净总回报（美元）指数期货",
+						"MSCI 新西兰净总回报（美元）指数期货",
+						"MSCI 太平洋除日本净总回报（美元）指数期货",
+						"MSCI 太平洋净总回报（美元）指数期货",
+						"MSCI 菲律宾净总回报（美元）指数期货",
+						"MSCI 新加坡自由净总回报（美元）指数期货",
+						"MSCI 新加坡净总回报（美元）指数期货",
+						"MSCI 台湾净总回报（美元）指数期货",
+						"MSCI 台湾25/50净总回报（美元）指数期货",
+						"MSCI 泰国净总回报（美元）指数期货",
+						"MSCI 越南净总回报（美元）指数期货"],
+						["MSCI 中国自由（美元）指数期货"],
+						["MSCI 印度（美元）指数期货"],
+						["MSCI 印尼（美元）指数期货"],
+						["MSCI 日本（日元）指数期货"],
+						["MSCI 马来西亚（美元）指数期货"],
+						["MSCI 菲律宾（美元）指数期货"],
+						["MSCI 新加坡自由（新加坡元）指数期货"],
+						["MSCI 台灣（美元）指數期貨",
+							"MSCI 台灣 25/50（美元）指數期貨"],
+						["MSCI 泰國（美元）指數期貨"],
+						["MSCI 越南（美元）指數期貨"],
+						["中华交易服务中国120 指数期货"],["-IBOVESPA期货"],["-MICEX指数期货"],["-FTSE/JSE Top40期货"],["一个月港元利率期货","三个月港元利率期货","美元兑人民币(香港)期货及期权"],["欧元兑人民币(香港)期货","日圆兑人民币(香港)期货","澳元兑人民币(香港)期货","人民币(香港)兑美元期货"],["美元及人民币(香港)伦敦铝/锌/铜/镍/锡/铅期货小型合约"],["美元及人民币(香港)黄金期货合约"],["铁矿石期货"]]
 
 app.get('/calendar/:type', function(req, res){
-
+	console.log(futuresListEng[0][0]);
 	url = 'https://www.hkex.com.hk/Services/Trading/Derivatives/Overview/Trading-Calendar-and-Holiday-Schedule?sc_lang=zh-HK';
 	request(url, function(error, response, html){
 		if(!error){
@@ -68,9 +162,12 @@ app.get('/calendar/:type', function(req, res){
 			for (var i = 0; i < filterList.length; i++) {
 				for (var j = 0; j < filterList[i].length; j++){
 					var descTc = filterList[i][j];
-					var descEn = futuresListEng[i][j];
-					var descSc = futuresListSc[i][j];
 					// console.log("[" + i + "][" + j + "]" + descTc);
+					var descEn = futuresListEng[i][j];
+					// console.log("descEn: " + descEn);
+					var descSc = futuresListSc[i][j];
+					// console.log("descSc: " + descSc);
+					 // console.log("[" + i + "][" + j + "]" + descTc);
 					var trLength = table_migrate.eq(index).find('tr').length;
 					if(trLength == '0'){
 						while (trLength <= 0) {
